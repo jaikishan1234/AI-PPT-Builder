5fba6b3db4ce2c245968bca6cfda347154608b25
Jai <jaikishannayak@gmail.com>
2026-05-14 16:55:14 +0530

    feat(auth): implement user authentication with better-auth and Prisma
    - Updated package.json to include better-auth and related dependencies.
    - Added User, Session, Account, and Verification models to Prisma schema.
    - Created migration SQL file for the new authentication tables.
    - Implemented auth client in auth-client.ts for better-auth integration.
    - Set up authentication logic in auth.ts using Prisma adapter and social providers.
    - Created API route for authentication handling in api/auth/$.

---
+++ src/lib/auth.ts
@@ -0,0 +1,21 @@
+import { prisma } from '#/db'
+import { betterAuth } from 'better-auth'
+import { tanstackStartCookies } from 'better-auth/tanstack-start'
+import { prismaAdapter } from 'better-auth/adapters/prisma'
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  socialProviders:{
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+    github:{
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }
+  },
+  plugins: [tanstackStartCookies()],
+})
